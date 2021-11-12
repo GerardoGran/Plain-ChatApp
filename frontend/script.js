814953006ec6ffa60b1fe5071f3497e8b346a333
Gerardo Granados Aldaz <gerardogr.al@outlook.com>
2021-11-12 12:20:04 -0700

    initial commit

---
+++ frontend/script.js
@@ -0,0 +1,37 @@
+const ENDPOINT = "https://localhost:2021";
+
+const socket = io(ENDPOINT);
+
+$(window).on("load", () => {
+  // Show modal on page load
+  $("#connection-modal").modal("show");
+});
+
+$("#ip-form").submit(function (e) {
+  // Hides modal, calls get /connect?host="{ip}"
+
+  e.preventDefault();
+
+  const ip = document.querySelector("#ip-input").value;
+
+  if (!validateIP(ip)) {
+    alert(`${ip} is not a valid IP\nXXX.XXX.XXX.XXX`);
+    return false;
+  }
+
+  /*
+  Call backend  
+  */
+
+  $("#connection-modal").modal("hide");
+});
+
+const validateIP = (ip) => {
+  // Validates IP against regex
+  return /^(?:[\d]{1,3}\.){3}[\d]{1,3}$/.test(ip);
+};
+
+const connect = async (ip) => {
+  const response = await fetch(`${REST_ENDPOINT}/conectar?host=http://${ip}`);
+  console.log(response);
+};
